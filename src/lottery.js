61a055e12924fa9926338cd4fbc3f4685f7c7708
MattLdwig <mattldwig@gmail.com>
2019-04-15 14:22:49 +0200

    conf($contract): Contract abi import

---
+++ src/lottery.js
@@ -0,0 +1,19 @@
+import web3 from './web3';
+
+const address = '0x27499C6FC72a9CE3b8CC241c13055FEC6DD97B2E';
+const abi = [{"constant":true,
+"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],
+"payable":false,"stateMutability":"view","type":"function"},
+{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],
+"payable":false,"stateMutability":"nonpayable","type":"function"},
+{"constant":true,"inputs":[],"name":"getPlayers",
+"outputs":[{"name":"","type":"address[]"}],"payable":false,
+"stateMutability":"view","type":"function"},
+{"constant":false,"inputs":[],"name":"enter","outputs":[],
+"payable":true,"stateMutability":"payable","type":"function"},
+{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players",
+"outputs":[{"name":"","type":"address"}],"payable":false,
+"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,
+"stateMutability":"nonpayable","type":"constructor"}];
+
+export default new web3.eth.Contract(abi, address);
