c4a191d218c707c78c6567ac18d5f280133596f5
MattLdwig <mattldwig@gmail.com>
2019-04-15 14:48:13 +0200

    feat($app): Add form input to enter the lottery

--- src/App.js
+++ src/App.js
@@ -6,19 +6,52 @@ import lottery from './lottery';
 
 
 class App extends Component {
-  constructor(props){
+  /*constructor(props){
     super(props);
     this.state = { manager: '' }
-  }
+  }*/
+  // ES6 Syntax
+  state = {
+    manager: '',
+    players: [],
+    balance: '',
+    value: ''
+  };
   async componentDidMount(){
     const manager = await lottery.methods.manager().call();
-    this.setState({ manager: manager });
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    this.setState({ manager, players, balance });
   }
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether')
+    });
+  }
+
   render() {
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p>This contract is manage by {this.state.manager}</p>
+        <p>This contract is manage by {this.state.manager}.</p>
+        <p>There are currently {this.state.players.length} people entered, 
+        competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!</p>
+        <hr />
+        <form onSubmit={this.onSubmit}>
+          <h4>Want to try your luck ?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input 
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value })}
+            />
+          </div>
+          <button>Enter</button>
+        </form>
       </div>
     );
   }
